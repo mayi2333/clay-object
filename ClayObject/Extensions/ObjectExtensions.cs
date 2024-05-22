2a403f1c3c8f165eb0b4315b5d7a6a909a6d14ec
Lqs <lddxdmy@qq.com>
2024-05-22 10:34:02 +0800

    添加项目文件。

---
+++ ClayObject/Extensions/ObjectExtensions.cs
@@ -0,0 +1,223 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.Json;
+
+namespace ClayObject.Extensions
+{
+
+    /// <summary>
+    /// 对象拓展类
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// 将 DateTimeOffset 转换成本地 DateTime
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTime ConvertToDateTime(this DateTimeOffset dateTime)
+        {
+            if (dateTime.Offset.Equals(TimeSpan.Zero))
+                return dateTime.UtcDateTime;
+            if (dateTime.Offset.Equals(TimeZoneInfo.Local.GetUtcOffset(dateTime.DateTime)))
+                return dateTime.ToLocalTime().DateTime;
+            else
+                return dateTime.DateTime;
+        }
+        /// <summary>
+        /// 将 DateTime 转换成 DateTimeOffset
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static DateTimeOffset ConvertToDateTimeOffset(this DateTime dateTime)
+        {
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Local);
+        }
+        /// <summary>
+        /// 判断是否是富基元类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        internal static bool IsRichPrimitive(this Type type)
+        {
+            // 处理元组类型
+            if (type.IsValueTuple()) return false;
+
+            // 处理数组类型，基元数组类型也可以是基元类型
+            if (type.IsArray) return type.GetElementType().IsRichPrimitive();
+
+            // 基元类型或值类型或字符串类型
+            if (type.IsPrimitive || type.IsValueType || type == typeof(string)) return true;
+
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) return type.GenericTypeArguments[0].IsRichPrimitive();
+
+            return false;
+        }
+        /// <summary>
+        /// 判断是否是元组类型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns></returns>
+        internal static bool IsValueTuple(this Type type)
+        {
+            return type.Namespace == "System" && type.Name.Contains("ValueTuple`");
+        }
+        /// <summary>
+        /// 判断类型是否实现某个泛型
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="generic">泛型类型</param>
+        /// <returns>bool</returns>
+        internal static bool HasImplementedRawGeneric(this Type type, Type generic)
+        {
+            // 检查接口类型
+            var isTheRawGenericType = type.GetInterfaces().Any(IsTheRawGenericType);
+            if (isTheRawGenericType) return true;
+
+            // 检查类型
+            while (type != null && type != typeof(object))
+            {
+                isTheRawGenericType = IsTheRawGenericType(type);
+                if (isTheRawGenericType) return true;
+                type = type.BaseType;
+            }
+
+            return false;
+
+            // 判断逻辑
+            bool IsTheRawGenericType(Type type) => generic == (type.IsGenericType ? type.GetGenericTypeDefinition() : type);
+        }
+        /// <summary>
+        /// 将一个对象转换为指定类型
+        /// </summary>
+        /// <param name="obj">待转换的对象</param>
+        /// <param name="type">目标类型</param>
+        /// <returns>转换后的对象</returns>
+        internal static object ChangeType(this object obj, Type type)
+        {
+            if (type == null) return obj;
+            if (type == typeof(string)) return obj?.ToString();
+            if (type == typeof(Guid) && obj != null) return Guid.Parse(obj.ToString());
+            if (type == typeof(bool) && obj != null && obj is not bool)
+            {
+                var objStr = obj.ToString().ToLower();
+                if (objStr == "1" || objStr == "true" || objStr == "yes" || objStr == "on") return true;
+                return false;
+            }
+            if (obj == null) return type.IsValueType ? Activator.CreateInstance(type) : null;
+
+            var underlyingType = Nullable.GetUnderlyingType(type);
+            if (type.IsAssignableFrom(obj.GetType())) return obj;
+            else if ((underlyingType ?? type).IsEnum)
+            {
+                if (underlyingType != null && string.IsNullOrWhiteSpace(obj.ToString())) return null;
+                else return Enum.Parse(underlyingType ?? type, obj.ToString());
+            }
+            // 处理 DateTime -> DateTimeOffset 类型
+            else if (obj.GetType().Equals(typeof(DateTime)) && (underlyingType ?? type).Equals(typeof(DateTimeOffset)))
+            {
+                return ((DateTime)obj).ConvertToDateTimeOffset();
+            }
+            // 处理 DateTimeOffset -> DateTime 类型
+            else if (obj.GetType().Equals(typeof(DateTimeOffset)) && (underlyingType ?? type).Equals(typeof(DateTime)))
+            {
+                return ((DateTimeOffset)obj).ConvertToDateTime();
+            }
+#if !NET5_0
+            // 处理 DateTime -> DateOnly 类型
+            else if (obj.GetType().Equals(typeof(DateTime)) && (underlyingType ?? type).Equals(typeof(DateOnly)))
+            {
+                return DateOnly.FromDateTime(((DateTime)obj));
+            }
+            // 处理 DateTime -> TimeOnly 类型
+            else if (obj.GetType().Equals(typeof(DateTime)) && (underlyingType ?? type).Equals(typeof(TimeOnly)))
+            {
+                return TimeOnly.FromDateTime(((DateTime)obj));
+            }
+#endif
+            else if (typeof(IConvertible).IsAssignableFrom(underlyingType ?? type))
+            {
+                try
+                {
+                    return Convert.ChangeType(obj, underlyingType ?? type, null);
+                }
+                catch
+                {
+                    return underlyingType == null ? Activator.CreateInstance(type) : null;
+                }
+            }
+            else
+            {
+                var converter = TypeDescriptor.GetConverter(type);
+                if (converter.CanConvertFrom(obj.GetType())) return converter.ConvertFrom(obj);
+
+                var constructor = type.GetConstructor(Type.EmptyTypes);
+                if (constructor != null)
+                {
+                    var o = constructor.Invoke(null);
+                    var propertys = type.GetProperties();
+                    var oldType = obj.GetType();
+
+                    foreach (var property in propertys)
+                    {
+                        var p = oldType.GetProperty(property.Name);
+                        if (property.CanWrite && p != null && p.CanRead)
+                        {
+                            property.SetValue(o, ChangeType(p.GetValue(obj, null), property.PropertyType), null);
+                        }
+                    }
+                    return o;
+                }
+            }
+
+            return obj;
+        }
+        /// <summary>
+        /// JsonElement 转 Object
+        /// </summary>
+        /// <param name="jsonElement"></param>
+        /// <returns></returns>
+        internal static object ToObject(this JsonElement jsonElement)
+        {
+            switch (jsonElement.ValueKind)
+            {
+                case JsonValueKind.String:
+                    return jsonElement.GetString();
+
+                case JsonValueKind.Undefined:
+                case JsonValueKind.Null:
+                    return default;
+
+                case JsonValueKind.Number:
+                    return jsonElement.GetDecimal();
+
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                    return jsonElement.GetBoolean();
+
+                case JsonValueKind.Object:
+                    var enumerateObject = jsonElement.EnumerateObject();
+                    var dic = new Dictionary<string, object>();
+                    foreach (var item in enumerateObject)
+                    {
+                        dic.Add(item.Name, item.Value.ToObject());
+                    }
+                    return dic;
+
+                case JsonValueKind.Array:
+                    var enumerateArray = jsonElement.EnumerateArray();
+                    var list = new List<object>();
+                    foreach (var item in enumerateArray)
+                    {
+                        list.Add(item.ToObject());
+                    }
+                    return list;
+
+                default:
+                    return default;
+            }
+        }
+    }
+}
